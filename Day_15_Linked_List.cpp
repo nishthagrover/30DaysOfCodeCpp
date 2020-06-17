60fa50c136b579af5d0448c5bbfa3d2a07d5bef9
Nishtha Grover <46939668+nishthagrover@users.noreply.github.com>
2020-06-17 16:59:39 +0530

    Day 15: Linked List
    Link to the complete problem statement: https://www.hackerrank.com/challenges/30-linked-list/problem

---
+++ Day_15_Linked_List.cpp
@@ -0,0 +1,65 @@
+// Link to the complete problem statement: https://www.hackerrank.com/challenges/30-linked-list/problem
+// Solution
+
+#include <iostream>
+#include <cstddef>
+using namespace std;	
+class Node
+{
+    public:
+        int data;
+        Node *next;
+        Node(int d)
+        {
+            data=d;
+            next=NULL;
+        }
+};
+class Solution
+{
+    public:
+
+        Node* insert(Node *head,int data) 
+        {
+          Node* tail = new Node(data);
+          if(head != NULL) 
+          {
+              Node *current = head;
+              while(current->next != NULL) 
+              {
+                  current = current->next;
+              }
+              current->next = tail;
+              return head;
+          } 
+          
+          else
+          {
+              return tail;
+          }
+      }
+
+      void display(Node *head)
+      {
+          Node *start=head;
+          while(start)
+          {
+              cout<<start->data<<" ";
+              start=start->next;
+          }
+      }
+};
+int main()
+{
+	Node* head=NULL;
+  	Solution mylist;
+    int T,data;
+    cin>>T;
+    while(T-->0)
+    {
+        cin>>data;
+        head=mylist.insert(head,data);
+    }	
+	mylist.display(head);
+		
+}
